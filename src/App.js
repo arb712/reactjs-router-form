8c05700a53d5cfe0de108af0b4323175cfba536b
arb712 <ariqbimantoro@gmail.com>
2020-04-14 10:32:34 +0700

    learn button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
+
+import ButtonSuccess from './components/TodoAPp/ButtonSuccess'
+import TodoApp from './components/TodoAPp';
+import Navbar1 from './components/TodoAPp/NavBar';
+
 
 function App() {
-  return (
-    <div>Saya mau belajar reactjs</div>
-  );
+    return (
+        <Container>
+          <Navbar1 />
+            <div>
+                <div>Saya mau belajar reactjs</div>
+                <ButtonSuccess name="tombolSetuju">Yes , Please</ButtonSuccess>
+                <TodoApp/>
+            </div>
+        </Container>
+
+    );
 }
 
 export default App;
---
+++ src/components/TodoAPp/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const index =() =>{
+    return (
+        <div>
+            <div>ini untuk input</div>
+            <div>ini untuk table</div>
+        </div>
+    )
+}
+
+export default index;
